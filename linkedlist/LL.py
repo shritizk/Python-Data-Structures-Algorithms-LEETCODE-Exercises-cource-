eed53fec2696213deffb100efd9f83d5fbcdd656
shritiz kumar <106963205+shritizk@users.noreply.github.com>
2025-05-20 00:02:46 +0100

    prepend function in LL

--- linkedlist/LL.py
+++ linkedlist/LL.py
@@ -37,7 +37,18 @@ class LinkedList:
             self.head = None
             self.tail = None 
         return temp
-         
+    
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length+=1
+        return True
+
             
 
 
