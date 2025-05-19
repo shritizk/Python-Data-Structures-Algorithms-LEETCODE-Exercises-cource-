6d0adcfe40f889190dff3aad6440c68ba943da29
shritiz kumar <106963205+shritizk@users.noreply.github.com>
2025-05-19 23:58:58 +0100

    pop function for LL

--- linkedlist/LL.py
+++ linkedlist/LL.py
@@ -21,6 +21,25 @@ class LinkedList:
         self.length +=1
         return True
     
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            pre = self.head
+            while(temp.next):
+                pre = temp 
+                temp = temp.next
+            self.tail = pre 
+            self.tail.next = None
+            self.length-=1
+        if self.length == 0:
+            self.head = None
+            self.tail = None 
+        return temp
+         
+            
+
 
 
 # practice
