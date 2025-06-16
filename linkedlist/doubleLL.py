2572fafdb4ec75b111779be43f96e39507799f7f
shritiz kumar <106963205+shritizk@users.noreply.github.com>
2025-06-16 20:46:29 +0100

    remove by index in doubly linked list is done

--- linkedlist/doubleLL.py
+++ linkedlist/doubleLL.py
@@ -112,3 +112,19 @@ class DoublyLinkedList :
         self.length +=1
         return True
             
+    def remove(self,index):
+        if index < 0 or index >= self.length :
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length -1 :
+            return self.pop()
+        
+        temp = self.get(index -1)
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+        self.length -=1
+        return temp 
+
